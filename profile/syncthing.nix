fff7f5f0543a8e0c54eff248f78e7b0b9ea8440b
Maciej Jur <maciej@kamoshi.org>
2025-04-17 19:10:09 +0200

    add syncthing folders

--- profile/syncthing.nix
+++ profile/syncthing.nix
@@ -16,18 +16,36 @@
         password = "kamov";
       };
       devices = {
-        "Arch" = {
+        "arch" = {
           id = "Q3QI722-QQL5J5Y-7MZJM54-BF2ZVYD-ZF25RJW-MH7OIJK-6QRCKBZ-KT6V4QF";
         };
-        "Xiaomi" = {
+        "xiaomi" = {
           id = "C5MES7V-2QNWNA2-AYGIW2L-CBXJ7QX-D2UPQP2-SM6EDAW-ZWMHDOV-MVTSUQN";
+        };
+      };
+      folders = {
+        "Photos" = {
+          id = "y963t-gpuhn";
+          path = "/home/kamov/Sync/Photos";
+          devices = [ "arch" "xiaomi" ];
+        };
+        "Obsidian" = {
+          id = "g2lzf-m5eu9";
+          path = "/home/kamov/Sync/Obsidian";
+          devices = [ "arch" "xiaomi" ];
         };
       };
     };
   };
 
+  # Syncthing ports: 8384 for remote access to GUI
+  # 22000 TCP and/or UDP for sync traffic
+  # 21027/UDP for discovery
+  # source: https://docs.syncthing.net/users/firewall.html
   networking.firewall = {
     enable = true;
+    allowedTCPPorts = [ 22000 ];
+    allowedUDPPorts = [ 22000 21027 ];
     interfaces.wg0.allowedTCPPorts = [ 8384 ];
   };
 
